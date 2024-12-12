cfab751309fa420cfde20ff5203e8eae2de0d6f2
HalilUslu33 <142406737+HalilUslu33@users.noreply.github.com>
2024-12-12 22:25:03 +0300

    film_uygulamasi.

--- lib/main.dart
+++ lib/main.dart
@@ -79,6 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 childAspectRatio: 2 / 3.5,
               ),
               itemBuilder: (context, indeks){
+
                 var film = filmlerListesi[indeks];
 
                 return GestureDetector(
